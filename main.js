ad98f85c9d8fafc6a8d6c30a8a4d31161f9cc81f
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-29 10:40:44 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,57 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+// create the main window
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Resizer',
+        width: isDev ? 1000 : 500,
+        height: 600,
+    });
+
+    // open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+// app is ready
+app.whenReady().then(() => {
+    createMainWindow();
+
+    // implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
+    
+});
+
+// menu template
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+                accelerator: 'CmdOrCtrl+Q'
+            }
+        ]
+    }
+]
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit();
+    }
+});
